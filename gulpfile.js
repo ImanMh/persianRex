dede65456003d177433491c98623488e10a0a98a
Iman Mohamadi <iman.mohamadi.dev@gmail.com>
2016-01-11 10:47:30 +0330

    dist folder structure changed to include all versions with correct naming acording to docs

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,12 @@
 var gulp = require('gulp'),
-    uglify = require('gulp-uglifyjs');
+    uglify = require('gulp-uglifyjs'),
+    rename = require('gulp-rename');
  
 gulp.task('default', function () {
 	return gulp.src('src/main.js').
+    pipe(rename('persian-rex.js')).
     pipe(gulp.dest('dist')).
-    pipe(uglify('persianRex.js', {
+    pipe(uglify('persian-rex.min.js', {
       outSourceMap: true
     })).
     pipe(gulp.dest('dist'));

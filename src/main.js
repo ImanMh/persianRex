6c3c4a390f0dcac80310aba7e35e5d55a82d22bf
Iman Mohamadi <iman.mohamadi.dev@gmail.com>
2016-01-13 11:01:41 +0330

    better names choosed for char and number ranges

--- src/main.js
+++ src/main.js
@@ -1,23 +1,23 @@
 (function () {
   persianRex = {};
   
-  var persianNumberRange = '[\u06F0-\u06F9]',
-      persianCharRange = ['[\\s,\u06A9\u06AF\u06C0\u06CC\u060C',
+  var numberRange = '[\u06F0-\u06F9]',
+      charRange = ['[\\s,\u06A9\u06AF\u06C0\u06CC\u060C',
 				'\u062A\u062B\u062C\u062D\u062E\u062F',
 				'\u063A\u064A\u064B\u064C\u064D\u064E',
 				'\u064F\u067E\u0670\u0686\u0698\u200C',
 				'\u0621-\u0629\u0630-\u0639\u0641-\u0654]'].join();
   
-  persianRex.number = new RegExp('^' + persianNumberRange +'+$');
-  persianRex.letter = new RegExp('^' + persianCharRange +'+$');
-  persianRex.text = new RegExp('^' + combineRegExps(persianNumberRange, persianCharRange) + '+$');
+  persianRex.number = new RegExp('^' + numberRange +'+$');
+  persianRex.letter = new RegExp('^' + charRange +'+$');
+  persianRex.text = new RegExp('^' + combineRegExps(numberRange, charRange) + '+$');
   
-  persianRex.hasNumber = new RegExp(persianNumberRange);
-  persianRex.hasLetter = new RegExp(persianCharRange);
-  persianRex.hasText = new RegExp(combineRegExps(persianNumberRange, persianCharRange));
+  persianRex.hasNumber = new RegExp(numberRange);
+  persianRex.hasLetter = new RegExp(charRange);
+  persianRex.hasText = new RegExp(combineRegExps(numberRange, charRange));
   
-  persianRex.lettersASCIRange = persianCharRange;
-  persianRex.numbersASCIRange = persianNumberRange;
+  persianRex.lettersASCIRange = charRange;
+  persianRex.numbersASCIRange = numberRange;
 
   function combineRegExps () {
 		var combined = '(';
@@ -43,8 +43,8 @@
     exports.hasNumber = persianRex.hasNumber;
     exports.hasLetter = persianRex.hasLetter;
     exports.hasText = persianRex.hasText;
-    exports.lettersASCIRange = persianCharRange;
-    exports.numbersASCIRange = persianNumberRange;
+    exports.lettersASCIRange = charRange;
+    exports.numbersASCIRange = numberRange;
     
   } else {
     window.persianRex = persianRex;  
